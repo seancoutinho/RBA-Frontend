26153b71d7c228ed2325faffbd9d0e2bcf7e02aa
Gashie <masogashie1@gmail.com>
2024-03-12 09:34:24 +0200

    jkm

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -26,7 +26,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async(user, thunkAPI
 
 export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) => {
     try {
-        const response = await axios.get('https://rba-backend-yi2x.onrender.com/products');
+        const response = await axios.get('https://rba-backend-yi2x.onrender.com/me');
         return response.data;
     } catch (error) {
         if(error.response){
