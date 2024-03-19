8c17ab28ed813d436923b8dbdd680358a272ac28
Gashie <masogashie1@gmail.com>
2024-03-19 12:30:45 +0200

    update

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 const initialState = {
     user: null,
